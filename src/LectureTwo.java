3194db24ce47b29191a97a2c105467022fc8f539
trasque <taisajazz@gmail.com>
2022-05-20 19:27:51 +0900

    不要なインスタンスを削除

--- src/LectureTwo.java
+++ src/LectureTwo.java
@@ -4,9 +4,6 @@ import java.util.List;
 public class LectureTwo {
   public static void main(String[] args) {
     Order mikan = new Order();
-    Order suika = new Order("すいか");
-    Order negi = new Order("ねぎ", 5);
-    Order kani = new Order("かに", 3, 800);
     Order saba = new Order("さば", 10, 120, "千葉");
 
     Fare tokyo = new Fare(1.1, "東京", 200);
