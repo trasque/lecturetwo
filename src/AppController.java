9f1ab8ad92bfbd97f895f68240bd3d29aaf10c28
trasque <taisajazz@gmail.com>
2022-05-20 23:26:08 +0900

    受注リストを専用クラス OrderList に分割しました
    - `OrderList` クラスで受注インスタンスをリスト化する
    - メインメニューでメソッドを呼び、読み込む構造とした

--- src/AppController.java
+++ src/AppController.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AppController {
@@ -8,20 +10,40 @@ public class AppController {
     mainMenu();
   }
 
-  private static int mainMenu() {
+  private static boolean mainMenu() {
+    List<Order> orderList = new ArrayList<>();
     Scanner mainMenuScanner = new Scanner(System.in);
-    int mainMenuNumber;
+    int mainMenuNumber = 1;
+    boolean mainMenuStatus = true;
 
-    System.out.println("[[メインメニュー]]");
-    System.out.println("1. 受注データ読み込み");
-    System.out.println("2. 受注件数表示");
-    System.out.println("3. 受注情報入力（個別）");
-    System.out.println("4. 受注情報表示（全て）");
-    System.out.println("5. 発送（個別）");
-    System.out.println("6. 発送（全て）");
-    System.out.print("◆受注プログラム機能を選択 > ");
-    mainMenuNumber = mainMenuScanner.nextInt();
+    while(mainMenuNumber != 0) {
+      System.out.println("\n[[メインメニュー]]");
+      System.out.println("1. 受注データ読み込み");
+      System.out.println("2. 受注件数表示");
+      System.out.println("3. 受注情報入力（個別）");
+      System.out.println("4. 受注情報表示（全て）");
+      System.out.println("5. 発送（個別）");
+      System.out.println("6. 発送（全て）");
+      System.out.println("0. 終了");
+      System.out.print("◆受注プログラム機能を選択 > ");
+      mainMenuNumber = mainMenuScanner.nextInt();
 
-    return mainMenuNumber;
+      switch(mainMenuNumber) {
+        case 1 -> {
+          orderList = OrderList.generateOrderList();
+          System.out.println("  受注リストを" + orderList.size() + "件よみこみました");
+        }
+        case 2 -> {
+          System.out.println("  受注リストは" + orderList.size() + "件です");
+        }
+        default -> {
+          mainMenuNumber = 0;
+        }
+      }
+    }
+
+    if (mainMenuNumber == 0) mainMenuStatus = true;
+    else mainMenuStatus = false;
+    return mainMenuStatus;
   }
 }
